873cbc24077a682538560a6245325ceccc19aa8d
ozanguldali <ozan.guldali@hotmail.com>
2018-05-16 00:00:34 +0300

    date interval improvements

--- src/gets/getDesiredDateInterval.R
+++ src/gets/getDesiredDateInterval.R
@@ -8,35 +8,23 @@ while ( repeatValue_date ) {
 
   if ( strWhich == "all" ) {
     printf <- function(...) cat(sprintf(...))
-    startDate <- "2018-02-27"
-    endDate_limit <- Sys.Date()
     
-    isDateBoolean_end <- FALSE
-    isLessThen_end <- FALSE
-    isValidInterval <- FALSE
+    libDir_date <- toString(getwd())
+    libDir_date <- paste(libDir_date, "lib", sep = "/")
     
-    while ( !isDateBoolean_end || !isLessThen_end || !isValidInterval ) {
-      strEnd <- readline(prompt = "Please enter the end date in the format (yyyy-mm-dd): ")
-      isDateBoolean_end <- IsDate( strEnd )
-      isLessThen_end <- strEnd <= endDate_limit
-      isValidInterval <- startDate <= strEnd
-      if ( isDateBoolean_end ) {
-        if ( isLessThen_end ) {
-          if ( isValidInterval ) {
-            endDate <- strEnd
-          } else {
-            printf("End date must be greater than start date %s\n", startDate)
-          }
-        } else {
-          printf("End date must be smaller than or equal to %s, since I am not a fortune teller :)\n", endDate_limit)
-        }
-      }
-    }
+    workspaceFolder_date <- list.files(path = libDir_date, pattern = "\\.RData", all.files = FALSE,
+                                      full.names = FALSE, recursive = FALSE,
+                                      ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+    workspaceFolder_date <- gsub("_coins_df.RData", "", gsub("20180227-", "", workspaceFolder_date))
 
+    startDate <- "2018-02-27"
+    endDate <- paste( substring(paste(substring(workspaceFolder_date, c(1,5), c(4,8)), collapse="-"), c(1,8), c(7,9)), collapse = "-" )
     
     repeatValue_date <- FALSE
+    
   } else if ( strWhich == "specific" ) {
     printf <- function(...) cat(sprintf(...))
+    
     startDate_limit <- "2018-02-27"
     endDate_limit <- Sys.Date()
     
@@ -85,10 +73,12 @@ while ( repeatValue_date ) {
     }
     
     repeatValue_date <- FALSE
+    
   } else {
     print("Please enter only \"all\" or \"specific\"")
 
     repeatValue_date <- TRUE
+    
   }
   
 }
